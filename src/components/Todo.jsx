07bc71042f140e16bc204a094f652e4bc46b3277
takuya_kajiwara <fd.elt.119@gmail.com>
2023-12-18 21:34:17 +0900

    tanStack-query実装、fetchでの挙動確認

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,11 +3,33 @@ import { useQuery } from '@tanstack/react-query';
 const fetchTodos = async () => {
   const res = await fetch('http://localhost:3001/todos');
 
+  if (!res.ok) {
+    const json = await res.json();
+    throw new Error(json.message);
+  }
+
   return res.json();
 };
 
 const Todo = () => {
-  const { data: todos } = useQuery({ queryKey: 'todos', queryFn: fetchTodos });
+  const {
+    data: todos,
+    error,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTodos,
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
   return (
     <>
       <h1>Todo一覧</h1>
